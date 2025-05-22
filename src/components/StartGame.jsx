be10534718d760e51221f7f024616ef316863a11
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-22 10:35:11 +0100

    feat: Initial commit

---
+++ src/components/StartGame.jsx
@@ -0,0 +1,11 @@
+import "../styles/Home.css";
+
+function StartGame() {
+	return (
+		<>
+			<button className="start-button"><span>Start Game</span></button>
+		</>
+	);
+}
+
+export default StartGame;
