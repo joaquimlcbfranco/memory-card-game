4091c8ea38ffa65d392ae4c9b9fc72d5fee92fc9
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-22 19:39:33 +0100

    feat: Create score section

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,20 +1,30 @@
+import { useState, useEffect } from "react"
 import Header from "../components/Header.jsx";
 import Card from "../components/Card.jsx";
-import "../styles/Game.css"
+import "../styles/Game.css";
 
-function Game({ difficulty, setDifficulty }) {
+function Game({ difficulty }) {
+	const [cards, setCards] = useState([]);
+
 	return (
 		<>
 			<div className="game-container">
 				<Header />
-        <div className="card-container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="score-container">
+          <h1>Score: 0/9</h1>
+          <h1>High Score: 9</h1>
         </div>
+				<div className="card-container">
+					<Card id="0"/>
+					<Card id="1"/>
+					<Card id="2"/>
+					<Card id="3"/>
+					<Card id="4"/>
+					<Card id="5"/>
+					<Card id="6"/>
+					<Card id="7"/>
+          <Card id="8"/>
+				</div>
 			</div>
 		</>
 	);
