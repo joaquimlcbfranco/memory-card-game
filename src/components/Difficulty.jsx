3e2e2fc303e2906fd95b0c75ea24350dff2469a2
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-22 10:37:10 +0100

    feat: Create onClick handler for each difficulty

--- src/components/Difficulty.jsx
+++ src/components/Difficulty.jsx
@@ -1,12 +1,21 @@
 import "../styles/Home.css";
 
 function Difficulty({ setDifficulty }) {
-  return (
-		<div className="difficulty-section">
-      <span><img src="https://placehold.co/150" onClick={setDifficulty("easy")} /><p>Easy</p></span>
-			<span><img src="https://placehold.co/150" onClick={setDifficulty("medium")} /><p>Medium</p></span>
-      <span><img src="https://placehold.co/150" onClick={setDifficulty("hard")} /><p>Hard</p></span>
-		</div>
+	return (
+		<>
+			<span>
+				<div onClick={() => setDifficulty("easy")} />
+				<p>Easy</p>
+			</span>
+			<span>
+				<div onClick={() => setDifficulty("medium")} />
+				<p>Medium</p>
+			</span>
+			<span>
+				<div onClick={() => setDifficulty("hard")} />
+				<p>Hard</p>
+			</span>
+		</>
 	);
 }
 
