3d206d3d55d3137e2688ce0cd3f77bf3f6194b09
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-22 10:38:49 +0100

    feat: Remove dropdown menu list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,8 @@ import "../styles/Header.css";
 function Header() {
 	return (
 		<div className="header-container">
-			<span className="header-title">
-        HOME
-			</span>
+			<p className="header-title">POKE<span>MEMORY</span></p>
+			<p className="header-description">Can you select them all?</p>
 		</div>
 	);
 }
