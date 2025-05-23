c106751840b0d6459e0b1f5b92fd60956589a5e2
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-23 12:42:03 +0100

    feat: Remove fetch commands, moved to Game.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,38 +1,18 @@
-import { useState, useEffect } from "react";
-
-function Card({ id }) {
-	const [pokemon, setPokemon] = useState(null);
-
-	useEffect(() => {
-		const getPokemonData = async () => {
-			const pokeId = Math.floor(Math.random() * 900);
-			const response = await fetch(
-				`https://pokeapi.co/api/v2/pokemon/${pokeId}`
-			);
-			const jsonData = await response.json();
-			setPokemon({
-				name: jsonData.name,
-				url: jsonData.sprites.front_default,
-			});
-		};
-
-		getPokemonData();
-	}, []);
-
-  if (!pokemon) {
+function Card({ id, name, imageUrl, handleCardClick, reset }) {
+  if (!name) {
     return (<h1>Loading...</h1>)
   }
 
-	if (pokemon) {
+	if (name) {
 		return (
-			<div className="card show" data-id={id}>
+			<div className="card show" data-id={id} onClick={() => handleCardClick(id)}>
 				<div
 					className="card-front card-content"
 					style={{
-						backgroundImage: `url(${pokemon.url})`,
+						backgroundImage: `url(${imageUrl})`,
 					}}
 				></div>
-				<p className="card-content">{pokemon.name}</p>
+				<p className="card-content">{name}</p>
 			</div>
 		);
 	}
