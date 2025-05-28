d1c0d86bd3ecc60ac153d25a961594a5f956edc5
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-28 12:01:13 +0100

    feat: Add text to indicate how the game is played

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
 	return (
 		<div className="header-container">
 			<p className="header-title">POKE<span>MEMORY</span></p>
-			<p className="header-description">Can you select them all?</p>
+			<p className="header-description">Can you select them all? <br></br><span>(Make sure not to click the same one twice)</span></p>
 		</div>
 	);
 }
